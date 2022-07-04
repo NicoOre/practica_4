e1fc4b55f2c10291b69238007b3f7d7583a2d93e
NicoOre <nicolasorellana619@gmail.com>
2022-07-04 08:43:40 -0500

    Creacion de cuadrado y botones

---
+++ juego.js
@@ -0,0 +1,39 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var movX = 0
+var movY = 0
+dibujar(ctx)
+
+function dibujar(ctx) {
+    ctx.fillStyle="red"
+    ctx.fillRect(movX, movY, 100, 100)
+}
+
+function moverDerecha() {
+    if (movX != 300) {
+        ctx.clearRect(movX, movY, 100, 100)
+        movX += 10
+        ctx.fillRect(movX, movY, 100, 100)
+    }
+
+}
+
+function moverAbajo() {
+    ctx.clearRect(movX, movY, 100, 100)
+    movY += 10
+    ctx.fillRect(movX, movY, 100, 100)
+}
+
+function moverIzquierda() {
+    if (movX != 0) {
+        ctx.clearRect(movX, movY, 100, 100)
+        movX -= 10
+        ctx.fillRect(movX, movY, 100, 100)
+    }
+}
+
+function moverArriba() {
+    ctx.clearRect(movX, movY, 100, 100)
+    movY -= 10
+    ctx.fillRect(movX, movY, 100, 100)
+}
